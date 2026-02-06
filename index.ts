5eafedb578e9182fe819f78a930a158c6dccc688
Alvaro Balbin <kamthegod34@gmail.com>
2026-02-06 01:38:38 +0100

    tryna fix errors

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 // index.ts (Deno server setup for Railway)
-import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
+import { serve } from "@std/http/server";
+
 
 const port = Number(Deno.env.get("PORT") ?? "8080");
 
